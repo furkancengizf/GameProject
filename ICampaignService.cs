deef33a2de990811958e63ac2f514b920c7bc8ad
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-02-13 15:03:10 +0300

    Create ICampaignService.cs

---
+++ ICampaignService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameIntro
+{
+    interface ICampaignService
+    {
+        void Add(Campaign campaign);
+        void Update(Campaign campaign);
+        void Delete(Campaign campaign);
+        
+    }
+}
