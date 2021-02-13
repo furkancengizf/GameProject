fafa8be291066f7602a7a5930b389e86090832b3
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-02-13 14:48:10 +0300

    Create Game.cs

---
+++ Game.cs
@@ -0,0 +1,14 @@
+namespace GameIntro
+{
+    partial class Program
+    {
+        public class Game
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+            
+        }
+        
+    }
+}
